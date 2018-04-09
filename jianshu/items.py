696f2791fed05679c8bdd87653c1f7d5b8779ace
lsz <qqlsz87@126.com>
2018-04-09 23:39:05 +0800

    jianshuwang

--- jianshu/items.py
+++ jianshu/items.py
@@ -5,10 +5,13 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item,Field
 
-
-class JianshuItem(scrapy.Item):
+class JianshuItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    name =Field()
+    content =Field()
+    article =Field()
+    fans =Field()
     pass
